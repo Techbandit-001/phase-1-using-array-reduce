e9841add96982f2a5149b7ec0a99efd7ce53ff6d
David Kiptum <davy@DESKTOP-7A71LJO.>
2025-03-19 22:05:56 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteriesBatches = [4, 5, 3, 4, 4, 6, 5, 6, 4, 7];
+
+const totalBatteries = batteryBatches.reduce(
+  (total, batch) => total + batch,
+  0
+);
